93ff3ea01e443fed2a2ac6ca640bbb462876e68f
HugoSantos14 <hugosantos14000@gmail.com>
2026-04-04 23:39:58 -0300

    refactor: rename transformed column to "name"

--- src/utils/etl_utils.py
+++ src/utils/etl_utils.py
@@ -27,27 +27,27 @@ def transform_multivalued_column(lf: pl.LazyFrame, column_name: str, id_name: st
     
     lf_exploded = (
         lf.select(["game_id", column_name])
-        .with_columns(clean_and_split_str(column_name))
-        .explode(column_name)
+        .with_columns(clean_and_split_str(column_name).alias("name"))
+        .explode("name")
         .with_columns(
-            pl.col(column_name)
+            pl.col("name")
             .str.strip_chars(" \r\n\t")
             .str.replace_all(r"\s+", " ")
         )
-        .filter((pl.col(column_name).is_not_null()) & (pl.col(column_name) != ""))
+        .filter((pl.col("name").is_not_null()) & (pl.col("name") != ""))
     )
     
     df_entity = (
-        lf_exploded.select(column_name)
+        lf_exploded.select("name")
         .unique()
-        .sort(column_name)
+        .sort("name")
         .collect()
         .with_row_index(name=id_name, offset=1)
     )
     
     df_associative = (
         lf_exploded.collect()
-        .join(df_entity, on=column_name, how="inner")
+        .join(df_entity, on="name", how="inner")
         .select(["game_id", id_name])
     )
     
