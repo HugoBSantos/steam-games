53aa6469d3b2c37086c7146e9499941155d877dc
HugoSantos14 <hugosantos14000@gmail.com>
2026-04-07 05:35:49 -0300

    refactor: update column types for consistency and improve schema definitions

--- sql/ddl/silver.sql
+++ sql/ddl/silver.sql
@@ -1,53 +1,52 @@
+DROP SCHEMA IF EXISTS silver CASCADE;
 CREATE SCHEMA IF NOT EXISTS silver;
 
 CREATE TABLE IF NOT EXISTS silver.supported_languages(
     language_id SERIAL PRIMARY KEY,
-    name VARCHAR(100) NOT NULL
+    name VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS silver.categories(
 	category_id SERIAL PRIMARY KEY,
-	name VARCHAR(100) NOT NULL
+	name VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS silver.tags(
 	tag_id SERIAL PRIMARY KEY,
-	name VARCHAR(100) NOT NULL
+	name VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS silver.developers(
-    developer_id SERIAL PRIMARY KEY, 
-    name VARCHAR(100) NOT NULL
+    developer_id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS silver.genres(
     genre_id SERIAL PRIMARY KEY,
-    name VARCHAR(100) NOT NULL
+    name VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS silver.publishers(
     publisher_id SERIAL PRIMARY KEY,
-    name VARCHAR(100) NOT NULL
+    name TEXT NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS silver.games(
 	game_id INT PRIMARY KEY,
-	developer_id INT REFERENCES silver.developers (developer_id),
-	publisher_id INT REFERENCES silver.publishers (publisher_id),
-	name VARCHAR(100) NOT NULL,
+	name TEXT NOT NULL,
 	release_date DATE NOT NULL,
-	estimated_owners VARCHAR(100) NOT NULL,
+	estimated_owners TEXT NOT NULL,
 	peak_ccu INT NOT NULL,
 	required_age SMALLINT NOT NULL,
-	price DECIMAL NOT NULL,
+	price DOUBLE PRECISION NOT NULL DEFAULT 0.0,
 	discount SMALLINT NOT NULL,
 	dlc_count SMALLINT NOT NULL,
 	about_the_game TEXT,
 	reviews TEXT,
-	header_image VARCHAR(100) NOT NULL,
-	website VARCHAR(100),
-	support_url VARCHAR(100),
-	support_email VARCHAR(100),
+	header_image TEXT NOT NULL,
+	website TEXT,
+	support_url TEXT,
+	support_email TEXT,
 	notes TEXT,
 	windows BOOLEAN NOT NULL,
 	mac BOOLEAN NOT NULL,
@@ -56,8 +55,8 @@ CREATE TABLE IF NOT EXISTS silver.games(
 
 CREATE TABLE IF NOT EXISTS silver.tags_games(
     game_id INT NOT NULL REFERENCES silver.games (game_id),
-    tags_id INT NOT NULL REFERENCES silver.tags (tag_id),
-    PRIMARY KEY(game_id, tags_id)
+    tag_id INT NOT NULL REFERENCES silver.tags (tag_id),
+    PRIMARY KEY(game_id, tag_id)
 );
 
 CREATE TABLE IF NOT EXISTS silver.game_genre(
@@ -69,7 +68,7 @@ CREATE TABLE IF NOT EXISTS silver.game_genre(
 CREATE TABLE IF NOT EXISTS silver.game_language(
     game_id INT NOT NULL REFERENCES silver.games (game_id),
     language_id INT NOT NULL REFERENCES silver.supported_languages (language_id),
-    has_full_audio BOOLEAN NOT NULL,
+    has_full_audio BOOLEAN NOT NULL DEFAULT false,
     PRIMARY KEY (game_id, language_id)
 );
 
@@ -95,7 +94,7 @@ CREATE TABLE IF NOT EXISTS silver.metrics(
 	metric_id SERIAL PRIMARY KEY,
 	game_id INT UNIQUE REFERENCES silver.games (game_id),
 	metacritic_score SMALLINT NOT NULL,
-	metacritic_url VARCHAR(100),
+	metacritic_url TEXT,
 	user_score SMALLINT NOT NULL,
 	positive INT NOT NULL,
 	negative INT NOT NULL,
@@ -111,11 +110,11 @@ CREATE TABLE IF NOT EXISTS silver.metrics(
 CREATE TABLE IF NOT EXISTS silver.screenshots(
     screenshot_id SERIAL PRIMARY KEY,
     game_id INT REFERENCES silver.games (game_id),
-    url VARCHAR(100) NOT NULL
+    url TEXT NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS silver.movies(
     movie_id SERIAL PRIMARY KEY,
     game_id INT REFERENCES silver.games (game_id),
-    url VARCHAR(100) NOT NULL
+    url TEXT NOT NULL
 );
