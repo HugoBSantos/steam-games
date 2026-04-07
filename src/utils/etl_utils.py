24f41013d48621f401b5c94f94a3dddbb62ac4d0
HugoSantos14 <hugosantos14000@gmail.com>
2026-04-07 05:36:00 -0300

    feat: implement PostgreSQL data loading with COPY command and update data handling

--- src/utils/etl_utils.py
+++ src/utils/etl_utils.py
@@ -1,20 +1,32 @@
 import polars as pl
 import psycopg
+from pathlib import Path
 import os
+import io
 
 def load_to_postgres(dataframes: list[tuple[str, pl.DataFrame]]):
     
     postgres_url = os.getenv("POSTGRES_URL")
+    ddl_path = Path().cwd() / "sql" / "ddl" / "silver.sql"
     
     with psycopg.connect(postgres_url) as conn:
+        with conn.cursor() as cur:
+            with open(ddl_path, mode="r") as f:
+                cur.execute(f.read())
+
+            for table_name, df in dataframes:
+                buffer = io.BytesIO()
+                
+                df.write_csv(buffer, include_header=False)
+                buffer.seek(0)
+                
+                columns_str = ", ".join(df.columns)
+                copy_sql = f"COPY silver.{table_name} ({columns_str}) FROM STDIN WITH (FORMAT csv)"
+                
+                with cur.copy(copy_sql) as copy:
+                    copy.write(buffer.read())
         
-        for table_name, df in dataframes:
-            df.write_database(
-                table_name=table_name,
-                connection=conn,
-                engine="adbc",
-                if_table_exists="replace"
-            )
+        conn.commit()
 
 def clean_and_split_str(column_name: str) -> pl.Expr:
     html_regex = r"&amp;lt;.*?&amp;gt;|<.*?>|&amp;[a-z]+;"
@@ -56,12 +68,18 @@ def transform_multivalued_column(lf: pl.LazyFrame, column_name: str, id_name: st
         .sort("name")
         .collect()
         .with_row_index(name=id_name, offset=1)
+        .with_columns(pl.col(id_name).cast(pl.Int32))
     )
     
     df_associative = (
         lf_exploded.collect()
         .join(df_entity, on="name", how="inner")
         .select(["game_id", id_name])
+        .unique(["game_id", id_name])
+        .with_columns(
+            pl.col("game_id").cast(pl.Int32),
+            pl.col(id_name).cast(pl.Int32)
+        )
     )
     
     return df_entity, df_associative
